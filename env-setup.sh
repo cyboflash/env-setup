32bd75ccd06b1b5d23ec59112fffbaed3ebb8358
flashburn <2373916+cyboflash@users.noreply.github.com>
2019-09-20 13:05:17 -0700

    Add cloning of dotfiles repo

--- env-setup.sh
+++ env-setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+git clone https://github.com/cyboflash/dotfiles.git /tmp/dotfiles
+mv /tmp/dotfiles .??* $HOME
+git submodule update --init --recursive
+git submodule update --recursive --remote
+
 if hostnamectl | grep -q Ubuntu; then
     ./remove-apt-vim.sh
     ./install-apt-packages.sh
